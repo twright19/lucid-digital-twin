4517d9c783f56b13dfe6cc4a4007c9091c959dfd
Travis Wright <traviswright@MacBook-Pro-4.local>
2025-02-09 20:48:09 -0500

    Fix TypeScript types for data state

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -1,21 +1,12 @@
-"use client"
+import React, { forwardRef } from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
+const Slider = forwardRef<HTMLSpanElement, React.ComponentProps<typeof SliderPrimitive.Root>>(
+  (props, ref) => (
+    <SliderPrimitive.Root ref={ref} className="relative flex w-full touch-none select-none items-center" {...props} />
+  )
+);
 
-const Slider = React.forwardRef((props, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className="relative flex w-full touch-none select-none items-center"
-    {...props}
-  >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-gray-200">
-      <SliderPrimitive.Range className="absolute h-full bg-blue-600" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-blue-600 bg-white" />
-  </SliderPrimitive.Root>
-))
+Slider.displayName = "Slider";
 
-Slider.displayName = "Slider"
-
-export { Slider }
+export default Slider;
